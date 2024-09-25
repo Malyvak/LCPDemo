36a68982bec88f82f23a363e9febb1145a27d572
unknown <malyvak@outlook.com>
2024-09-25 04:27:32 +0100

    updated vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/LCPDemo/",
   plugins: [
     vue(),
     Components({
